439c9a07d90baa3fb7c4ffd65d9b9143475e7a33
Taro <49375207+tarowillmakeitright@users.noreply.github.com>
2021-12-12 14:45:19 +0900

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "林純平";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Hayashi Jyunpei";
 
 export default function Layout({ children, home }) {
   return (
